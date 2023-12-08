462352179e5b45ed87c72031aafac63979947bb6
paresiqbal <thecrowdel@gmail.com>
2023-12-08 15:04:35 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="grid place-content-center h-screen">
+      <h1>Vite Is Awesome</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
