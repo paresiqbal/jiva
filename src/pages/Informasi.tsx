02b6379a43f2877de52c25a368b27b80f83ae3b9
paresiqbal <thecrowdel@gmail.com>
2023-12-09 14:06:31 +0700

    Kecemasan

--- src/pages/Informasi.tsx
+++ src/pages/Informasi.tsx
@@ -1,3 +1,54 @@
 export default function Informasi() {
-  return <div>Informasi</div>;
+  return (
+    <div>
+      <h1 class="text-3xl font-bold mb-4">LAYANAN INFORMASI</h1>
+
+      <div class="mb-6">
+        <h2 class="text-2xl font-bold mb-2">1. Respons Fisiologis</h2>
+        <p>
+          Kecemasan memicu reaksi "lawan atau lari" dalam tubuh, menyebabkan
+          peningkatan detak jantung, pernapasan yang cepat, dan peningkatan
+          aliran darah ke otot-otot.
+        </p>
+      </div>
+
+      <div class="mb-6">
+        <h2 class="text-2xl font-bold mb-2">2. Gangguan Kecemasan</h2>
+        <p>
+          Ini termasuk gangguan kecemasan umum, gangguan panik, fobia spesifik,
+          gangguan kecemasan sosial, dan gangguan stres pasca-trauma. Setiap
+          jenis memiliki gejala dan pemicu yang berbeda.
+        </p>
+      </div>
+
+      <div class="mb-6">
+        <h2 class="text-2xl font-bold mb-2">
+          6. Pentingnya Bantuan Profesional
+        </h2>
+        <p>
+          Mendapatkan bantuan dari profesional kesehatan mental sangat penting
+          dalam mengelola kecemasan, terutama jika itu berdampak signifikan pada
+          kehidupan sehari-hari seseorang.
+        </p>
+      </div>
+
+      <p class="text-lg">
+        Mengenali dan mengatasi kecemasan adalah langkah penting dalam menjaga
+        kesehatan mental dan kualitas hidup.
+      </p>
+
+      <p class="text-lg">
+        Faktor-faktor yang dapat menyebabkan atau memperburuk kecemasan beragam,
+        mencakup elemen biologis, psikologis, lingkungan, dan gaya hidup.
+      </p>
+
+      <p class="text-lg">
+        Penting untuk diingat bahwa pendekatan yang efektif bervariasi untuk
+        setiap individu. Jika kecemasan Anda parah atau mengganggu kehidupan
+        sehari-hari, sangat disarankan untuk mencari bantuan profesional. Dokter
+        atau terapis dapat memberikan bimbingan dan rekomendasi yang disesuaikan
+        dengan kebutuhan spesifik Anda.
+      </p>
+    </div>
+  );
 }
