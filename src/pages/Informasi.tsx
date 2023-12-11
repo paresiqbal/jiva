3253cc818e29e9c75d43ef499a756a78ab2e52dd
paresiqbal <thecrowdel@gmail.com>
2023-12-11 23:06:46 +0700

    Add acirdion

--- src/pages/Informasi.tsx
+++ src/pages/Informasi.tsx
@@ -1,54 +1,139 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Informasi() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold mb-4">LAYANAN INFORMASI</h1>
-
-      <div class="mb-6">
-        <h2 class="text-2xl font-bold mb-2">1. Respons Fisiologis</h2>
+    <div className="flex flex-col gap-8">
+      <h1 class="text-3xl font-bold">LAYANAN INFORMASI</h1>
+      <section>
         <p>
-          Kecemasan memicu reaksi "lawan atau lari" dalam tubuh, menyebabkan
-          peningkatan detak jantung, pernapasan yang cepat, dan peningkatan
-          aliran darah ke otot-otot.
+          <span className="font-bold">Kecemasan</span> adalah perasaan
+          ketakutan, kekhawatiran, atau gugup yang sering muncul sebagai reaksi
+          terhadap stres atau ketidakpastian. Walaupun merupakan respons alami
+          terhadap situasi tertentu, kecemasan yang berlebihan atau
+          berkelanjutan bisa menjadi tanda gangguan kecemasan. Berikut adalah
+          beberapa aspek penting tentang kecemasan:
         </p>
-      </div>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Respons Fisiologis</AccordionTrigger>
+            <AccordionContent>
+              Kecemasan memicu reaksi "lawan atau lari" dalam tubuh, menyebabkan
+              peningkatan detak jantung, pernapasan yang cepat, dan peningkatan
+              aliran darah ke otot-otot.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Gangguan Kecemasan:</AccordionTrigger>
+            <AccordionContent>
+              Ini termasuk gangguan kecemasan umum, gangguan panik, fobia
+              spesifik, gangguan kecemasan sosial, dan gangguan stres
+              pasca-trauma. Setiap jenis memiliki gejala dan pemicu yang
+              berbeda.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Gejala Umum:</AccordionTrigger>
+            <AccordionContent>
+              Termasuk kekhawatiran berlebihan, sulit konsentrasi, iritabilitas,
+              otot tegang, gangguan tidur, dan kelelahan. Gejala fisik juga bisa
+              termasuk mual, diare, atau berkeringat.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>Penyebab:</AccordionTrigger>
+            <AccordionContent>
+              Kecemasan bisa disebabkan oleh berbagai faktor, termasuk genetika,
+              kimia otak, lingkungan, dan pengalaman hidup.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger>Pengobatan:</AccordionTrigger>
+            <AccordionContent>
+              Terapi, seperti terapi perilaku kognitif (CBT), dan kadang-kadang
+              obat-obatan, digunakan untuk mengelola kecemasan. Praktik
+              relaksasi seperti meditasi, yoga, dan olahraga teratur juga dapat
+              membantu.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-6">
+            <AccordionTrigger>Pentingnya Bantuan Profesional:</AccordionTrigger>
+            <AccordionContent>
+              Mendapatkan bantuan dari profesional kesehatan mental sangat
+              penting dalam mengelola kecemasan, terutama jika itu berdampak
+              signifikan pada kehidupan sehari-hari seseorang.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
 
-      <div class="mb-6">
-        <h2 class="text-2xl font-bold mb-2">2. Gangguan Kecemasan</h2>
+      <section>
         <p>
-          Ini termasuk gangguan kecemasan umum, gangguan panik, fobia spesifik,
-          gangguan kecemasan sosial, dan gangguan stres pasca-trauma. Setiap
-          jenis memiliki gejala dan pemicu yang berbeda.
+          <span className="font-bold">Mengenali dan mengatasi kecemasan</span>{" "}
+          adalah langkah penting dalam menjaga kesehatan mental dan kualitas
+          hidup. Pendekatan eksistensial (Corey, 1996:178) melihat ada dua jenis
+          kecemasan, yaitu kecemasan biasa (normal anxiety) dan kecemasan
+          neurotik (neurotic anxiety).
         </p>
-      </div>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Kecemasan Biasa</AccordionTrigger>
+            <AccordionContent>
+              Normal anxiety merupakan tanggapan yang cukup wajar terhadap
+              peristiwa yang sedang dihadapi, kecemasan ini tidak perlu
+              dihilangkan sebab ini sebagai motivasi ke arah perubahan.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Kecemasan Neurotik</AccordionTrigger>
+            <AccordionContent>
+              Neurotic anxiety adalah kecemasan yang keluar dari proporsi yang
+              ada, ia terjadi di luar kesadaran dan cenderung untuk menjadikan
+              orang tidak memiliki keseimbangan.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
 
-      <div class="mb-6">
-        <h2 class="text-2xl font-bold mb-2">
-          6. Pentingnya Bantuan Profesional
-        </h2>
+      <section>
         <p>
-          Mendapatkan bantuan dari profesional kesehatan mental sangat penting
-          dalam mengelola kecemasan, terutama jika itu berdampak signifikan pada
-          kehidupan sehari-hari seseorang.
+          Sigmund Freud (dalam Corey, 1996: 95) mengemukakan, bahwa kecemasan
+          adalah keadaan tegang yang memaksa untuk berbuat sesuatu. Ada tiga
+          jenis kecemasan, yaitu; kecemasan realita (rality anxiety), kecemasan
+          neurotik (neurotic anxiety), dan kecemasan moral (moral anxiety).
         </p>
-      </div>
-
-      <p class="text-lg">
-        Mengenali dan mengatasi kecemasan adalah langkah penting dalam menjaga
-        kesehatan mental dan kualitas hidup.
-      </p>
-
-      <p class="text-lg">
-        Faktor-faktor yang dapat menyebabkan atau memperburuk kecemasan beragam,
-        mencakup elemen biologis, psikologis, lingkungan, dan gaya hidup.
-      </p>
-
-      <p class="text-lg">
-        Penting untuk diingat bahwa pendekatan yang efektif bervariasi untuk
-        setiap individu. Jika kecemasan Anda parah atau mengganggu kehidupan
-        sehari-hari, sangat disarankan untuk mencari bantuan profesional. Dokter
-        atau terapis dapat memberikan bimbingan dan rekomendasi yang disesuaikan
-        dengan kebutuhan spesifik Anda.
-      </p>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Kecemasan Realita</AccordionTrigger>
+            <AccordionContent>
+              Rality anxiety, adalah rasa takut akan bahaya yang datang dari
+              dunia luar, dan derajat kecemasan semacam itu sesuai dengan
+              tingkat ancaman yang nyata.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Kecemasan Neurotik</AccordionTrigger>
+            <AccordionContent>
+              Neurotic anxiety, adalah rasa takut kalau-kalau insting akan
+              keluar jalur dan menyebabkan seseorang berbuat sesuatu yang akan
+              menyebabkan ia dihukum.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Kecemasan Moral</AccordionTrigger>
+            <AccordionContent>
+              Moral anxiety, adalah rasa takut terhadap hati nuraninya sendiri.
+              Orang dengan hati nuraninya yang cukup berkembang cenderung untuk
+              merasa bersalah apabila mereka berbuat sesuatu yang bertentangan
+              dengan kode moral mereka atau dengan introyeksi ibu bapa mereka.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
     </div>
   );
 }
