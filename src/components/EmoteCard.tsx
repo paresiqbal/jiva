70f02374767fce5ced2595db1425262f8ffcc771
paresiqbal <thecrowdel@gmail.com>
2023-12-13 13:34:31 +0700

    asdasd

--- src/components/EmoteCard.tsx
+++ src/components/EmoteCard.tsx
@@ -2,17 +2,29 @@ import { useState } from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 // Import your images
-import anxiety from "@/assets/emoji/anxiety.png";
-import fear from "@/assets/emoji/fear.png";
-import hearing from "@/assets/emoji/hearing.png";
-import personality from "@/assets/emoji/personality.png";
-import therapy from "@/assets/emoji/therapy.png";
+import panik from "@/assets/emoji/panik.png";
+import kecemasanBerat from "@/assets/emoji/kecemasanBerat.png";
+import kecemasanSedang from "@/assets/emoji/kecemasanSedang.png";
+import kecemasanRingan from "@/assets/emoji/kecemasanRingan.png";
+import keadaanBaik from "@/assets/emoji/keadaanBaik.png";
 
 export default function EmoteCard({ onSelect }) {
   const [selectedCard, setSelectedCard] = useState(null);
 
-  const images = [anxiety, fear, hearing, personality, therapy];
-  const names = ["Anxiety", "Fear", "Hearing", "Personality", "Therapy"];
+  const images = [
+    panik,
+    kecemasanBerat,
+    kecemasanSedang,
+    kecemasanRingan,
+    keadaanBaik,
+  ];
+  const names = [
+    "Panik",
+    "Kecemasan Berat",
+    "Kecemasan Sedang",
+    "Kecemasan Ringan",
+    "Keadaan Baik",
+  ];
 
   const handleClick = (index) => {
     setSelectedCard(index);
@@ -33,13 +45,11 @@ export default function EmoteCard({ onSelect }) {
             <img
               src={image}
               alt={`${names[index]} Image`}
-              className="w-20 h-20"
+              className="w-20 h-20 items-center"
             />
           </CardHeader>
           <CardContent>
-            <h2 className="font-bold">
-              {names[index]} {index + 1}
-            </h2>
+            <h2 className="font-bold">{names[index]}</h2>
           </CardContent>
         </Card>
       ))}
