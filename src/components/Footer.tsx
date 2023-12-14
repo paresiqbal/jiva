1cd0242c9ef4c41b5db1e394d83753380b3129ce
paresiqbal <thecrowdel@gmail.com>
2023-12-14 22:35:38 +0700

    Fix Footer UI Desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <h2 className="font-custom text-3xl font-bold">&copy; JIVA</h2>
         <div className="my-4">
           <h3 className="font-semibold text-xl pb-2">Contact</h3>
-          <div className="flex flex-col md:flex-row justify-between">
+          <div className="flex flex-col md:flex-row gap-2">
             <div className="flex items-center gap-2 font-semibold mb-2 md:mb-0">
               <FaInstagram className="w-6 h-6" />
               <a
@@ -41,9 +41,9 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col md:flex-row gap-2 md:gap-8">
           <p className="mb-2 md:mb-0">
-            <span className="font-semibold">Email:</span>{" "}
+            <span className="font-semibold">Email:</span>
             <a href="mailto:contact@jiva.id">jivaforyouanxiety@gmail.com</a>
           </p>
           <p className="mb-2 md:mb-0">
