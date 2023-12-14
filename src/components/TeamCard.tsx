1b3f215141771100fdd6c25988ba99ab85f42403
paresiqbal <thecrowdel@gmail.com>
2023-12-14 22:43:53 +0700

    Fix the card desktorp layout

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -12,7 +12,7 @@ import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 export default function TeamCard() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
