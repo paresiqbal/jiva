91ef6e836c657c221fc8c541e687dd220c1f8a14
paresiqbal <thecrowdel@gmail.com>
2023-12-14 23:18:32 +0700

    Add slider

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -9,30 +9,111 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
 export default function FeedbackCard() {
+  const breakpoints = {
+    320: { slidesPerView: 1 }, // Mobile view
+    768: { slidesPerView: 2 }, // Medium view
+    1024: { slidesPerView: 3 }, // Large view
+  };
+
   return (
     <div>
-      <Card className="flex flex-col items-center text-center">
-        <CardHeader className="flex flex-col items-center">
-          <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
-          </Avatar>
-          <CardTitle>John Doe</CardTitle>
-          <CardDescription>johndoe@email.com</CardDescription>
-        </CardHeader>
-        <CardContent className="text-sm">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat,
-            vero?
-          </p>
-        </CardContent>
-        {/* <CardFooter>
-          <p>johndoe@gmail.com</p>
-        </CardFooter> */}
-      </Card>
+      <Swiper
+        spaceBetween={50}
+        breakpoints={breakpoints}
+        touchEventsTarget="wrapper" // Ensure proper touch interaction
+        simulateTouch={true} // Enable touch simulation
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <Card className="flex flex-col items-center text-center">
+            <CardHeader className="flex flex-col items-center">
+              <Avatar className="items-center">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="mx-auto"
+                />
+              </Avatar>
+              <CardTitle>John Doe</CardTitle>
+              <CardDescription>johndoe@email.com</CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellat, vero?
+              </p>
+            </CardContent>
+          </Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card className="flex flex-col items-center text-center">
+            <CardHeader className="flex flex-col items-center">
+              <Avatar className="items-center">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="mx-auto"
+                />
+              </Avatar>
+              <CardTitle>John Doe</CardTitle>
+              <CardDescription>johndoe@email.com</CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellat, vero?
+              </p>
+            </CardContent>
+          </Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card className="flex flex-col items-center text-center">
+            <CardHeader className="flex flex-col items-center">
+              <Avatar className="items-center">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="mx-auto"
+                />
+              </Avatar>
+              <CardTitle>John Doe</CardTitle>
+              <CardDescription>johndoe@email.com</CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellat, vero?
+              </p>
+            </CardContent>
+          </Card>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Card className="flex flex-col items-center text-center">
+            <CardHeader className="flex flex-col items-center">
+              <Avatar className="items-center">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="mx-auto"
+                />
+              </Avatar>
+              <CardTitle>John Doe</CardTitle>
+              <CardDescription>johndoe@email.com</CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repellat, vero?
+              </p>
+            </CardContent>
+          </Card>
+        </SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 }
