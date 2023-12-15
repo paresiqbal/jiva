04e315ce71dc2cec1c716a941eec1bc3734c5703
paresiqbal <thecrowdel@gmail.com>
2023-12-15 12:46:06 +0700

    asdas

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,8 @@
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <div>
+      <h1>404 Page not Found</h1>
+      <h2>Ups, Sepertinya Halaman yang Kamu Cari Tidak Aada</h2>
+    </div>
+  );
 }
