f1c1de1987ed6592241627b48233ebdc06326d90
paresiqbal <thecrowdel@gmail.com>
2023-12-15 14:54:12 +0700

    Add page number

--- src/pages/Article/Artikel.tsx
+++ src/pages/Article/Artikel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Card,
@@ -19,32 +20,66 @@ const dummyData: Article[] = [
     description:
       "Kekhawatiran ibarat tamu tak diundang dalam pikiran kita, sering kali menyelinap masuk di saat yang tidak kita duga.",
     date: "12 Des, 2023",
-    id: 1, // Add ID for each article
+    id: 1,
   },
   {
     title: "Strategi Mengatasi Kekhawatiran",
     description:
       "Ketika kekhawatiran mulai membebani kita, penting bagi kita untuk mempunyai strategi untuk mengatasinya. Terlibat dalam aktivitas kesadaran, seperti meditasi",
     date: "07 Oct, 2023",
-    id: 2, // Add ID for each article
+    id: 2,
   },
   {
     title: "Merangkul Perubahan Positif di Tengah Kekhawatiran",
     description:
       "Kekhawatiran sering kali menyertai saat-saat perubahan, namun hal tersebut tidak harus mendikte pengalaman kita. Daripada melihat perubahan hanya melalui kacamata kecemasan, kita bisa melakukannya",
     date: "30 Arp, 2023",
-    id: 3, // Add ID for each article
+    id: 3,
+  },
+  {
+    title: "Merangkul Perubahan Positif di Tengah Kekhawatiran",
+    description:
+      "Kekhawatiran sering kali menyertai saat-saat perubahan, namun hal tersebut tidak harus mendikte pengalaman kita. Daripada melihat perubahan hanya melalui kacamata kecemasan, kita bisa melakukannya",
+    date: "30 Arp, 2023",
+    id: 4,
+  },
+  {
+    title: "Merangkul Perubahan Positif di Tengah Kekhawatiran",
+    description:
+      "Kekhawatiran sering kali menyertai saat-saat perubahan, namun hal tersebut tidak harus mendikte pengalaman kita. Daripada melihat perubahan hanya melalui kacamata kecemasan, kita bisa melakukannya",
+    date: "30 Arp, 2023",
+    id: 5,
+  },
+  {
+    title: "Merangkul Perubahan Positif di Tengah Kekhawatiran",
+    description:
+      "Kekhawatiran sering kali menyertai saat-saat perubahan, namun hal tersebut tidak harus mendikte pengalaman kita. Daripada melihat perubahan hanya melalui kacamata kecemasan, kita bisa melakukannya",
+    date: "30 Arp, 2023",
+    id: 6,
   },
 ];
 
+const ITEMS_PER_PAGE = 3;
+
 export default function Artikel() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const lastIndex = currentPage * ITEMS_PER_PAGE;
+  const firstIndex = lastIndex - ITEMS_PER_PAGE;
+  const currentArticles = dummyData.slice(firstIndex, lastIndex);
+
+  const totalPages = Math.ceil(dummyData.length / ITEMS_PER_PAGE);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="px-6 bg-white py-12">
       <h1 className="text-3xl font-bold mb-4">ARTIKEL</h1>
       <div>
-        {dummyData.map((data) => (
+        {currentArticles.map((data) => (
           <Link to={`/ArtikelDetail/${data.id}`} key={data.id}>
-            {/* Wrap each Card in a Link */}
             <Card className="mb-4">
               <CardHeader>
                 <CardTitle className="text-lg underline hover:cursor-pointer font-semibold">
@@ -57,18 +92,18 @@ export default function Artikel() {
           </Link>
         ))}
       </div>
-      <div className="flex justify-center items-center mt-4">
-        <div className="flex">
-          <span className="mr-2">Page:</span>
-          {dummyData.map((_, index) => (
-            <span
-              key={index}
-              className="mx-1 px-3 py-1 border border-gray-300 rounded hover:bg-gray-200 cursor-pointer"
-            >
-              {index + 1}
-            </span>
-          ))}
-        </div>
+      <div className="flex justify-center mt-4">
+        {Array.from(Array(totalPages).keys()).map((pageNumber) => (
+          <button
+            key={pageNumber + 1}
+            onClick={() => handlePageChange(pageNumber + 1)}
+            className={`mx-2 py-1 px-3 rounded-lg ${
+              currentPage === pageNumber + 1 ? "bg-gray-300" : "bg-gray-100"
+            }`}
+          >
+            {pageNumber + 1}
+          </button>
+        ))}
       </div>
     </div>
   );
