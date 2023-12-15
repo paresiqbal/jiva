938a664a84d3dbd7f847c81d00faceab6b2c3ee3
paresiqbal <thecrowdel@gmail.com>
2023-12-15 15:19:09 +0700

    Fix style in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,61 +27,70 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="text-xl bg-white px-6 py-12">
-      <div className="text-center">
-        <h1 className="text-8xl lg:text-9xl font-extrabold font-custom py-12">
-          JIVA
-        </h1>
-        <p className="text-xl lg:text-2xl text-gray-800 font-bold">
-          Eksplorasi mendalam tentang kecemasan: definisi, jenis, gejala, dan
-          cara mengelolanya. Temukan teknik pernapasan, meditasi, olahraga, dan
-          strategi relaksasi di JIVA. Sumber terpercaya untuk referensi yang
-          dapat diandalkan.
-        </p>
-      </div>
-      <div className="py-12 flex flex-col justify-center gap-4 items-center border-b-2">
-        <Button onClick={scrollToContent} className="px-10 py-6 text-xl">
-          Kenapa JIVA ?
-        </Button>
-        <div className="flex justify-between gap-2">
-          <Button onClick={scrollToContact} variant="outline">
-            Hubungi Kami
-          </Button>
-          <a href="/mood">
-            <Button
-              variant="outline"
-              className="hover:bg-primary hover:text-white"
-            >
-              Tes Kecemasanmu Hari ini ?
-            </Button>
-          </a>
+    <div className="text-xl py-12 flex flex-col gap-8">
+      <section className="bg-white rounded-md px-6">
+        <div className="text-center ">
+          <h1 className="text-8xl lg:text-9xl font-extrabold font-custom py-12">
+            JIVA
+          </h1>
+          <p className="text-xl lg:text-2xl text-gray-800 font-bold">
+            Eksplorasi mendalam tentang kecemasan: definisi, jenis, gejala, dan
+            cara mengelolanya. Temukan teknik pernapasan, meditasi, olahraga,
+            dan strategi relaksasi di JIVA. Sumber terpercaya untuk referensi
+            yang dapat diandalkan.
+          </p>
         </div>
-      </div>
-      <div
-        ref={contentRef}
-        className="mx-auto flex pt-16 pb-10 flex-col lg:flex-row items-center justify-center gap-8 lg:gap-32 border-b-2"
-      >
-        <p className="lg:w-1/2 text-md">
-          JIVA berasal dari Bahasa Indonesia yang berarti jiwa. Jiwa sendiri
-          berarti nyawa, zat hidup, kehidupan, makhluk hidup, ruh manusia, ruh
-          individual. Dari sudut pandang sejarah, kata jiwa dan mental tidak
-          cuma erat kaitannya dengan "hidup" dan "kehidupan manusia" tetapi juga
-          dengan "pikiran" dan esensi sebagai "manusia" itu sendiri. Dalam
-          Bahasa Inggris, kata mental health atau kesehatan jiwa itu sendiri,
-          dalam arti jiwa atau mental yang erat kaitannya dengan kesejahteraan
-          batiniah manusia, mulai digunakan sejak tahun 1803.
-        </p>
-        <img className="lg:w-1/6 sm:w-1/4 pb-2" src={loveImage} alt="love" />
-      </div>
-      <div className="py-8 border-b-2">
-        <h2 className="text-3xl font-semibold pb-8 text-center">Our Team</h2>
-        <TeamCard />
-      </div>
+        <div className="py-12 flex flex-col justify-center gap-4 items-center border-b-2 ">
+          <Button onClick={scrollToContent} className="px-10 py-6 text-xl">
+            Kenapa JIVA ?
+          </Button>
+          <div className="flex justify-between gap-2">
+            <Button onClick={scrollToContact} variant="outline">
+              Hubungi Kami
+            </Button>
+            <a href="/mood">
+              <Button
+                variant="outline"
+                className="hover:bg-primary hover:text-white"
+              >
+                Tes Kecemasanmu Hari ini ?
+              </Button>
+            </a>
+          </div>
+        </div>
+      </section>
 
-      <div className="text-2xl font-semibold text-center">
-        <h2 className="text-3xl py-8">Testimoni</h2>
-        <FeedbackCard />
-      </div>
+      <section className="bg-white rounded-md px-6">
+        <div
+          ref={contentRef}
+          className="mx-auto flex pt-16 pb-10 flex-col lg:flex-row items-center justify-center gap-8 lg:gap-32 border-b-2 "
+        >
+          <p className="lg:w-1/2 text-md">
+            JIVA berasal dari Bahasa Indonesia yang berarti jiwa. Jiwa sendiri
+            berarti nyawa, zat hidup, kehidupan, makhluk hidup, ruh manusia, ruh
+            individual. Dari sudut pandang sejarah, kata jiwa dan mental tidak
+            cuma erat kaitannya dengan "hidup" dan "kehidupan manusia" tetapi
+            juga dengan "pikiran" dan esensi sebagai "manusia" itu sendiri.
+            Dalam Bahasa Inggris, kata mental health atau kesehatan jiwa itu
+            sendiri, dalam arti jiwa atau mental yang erat kaitannya dengan
+            kesejahteraan batiniah manusia, mulai digunakan sejak tahun 1803.
+          </p>
+          <img className="lg:w-1/6 sm:w-1/4 pb-2" src={loveImage} alt="love" />
+        </div>
+      </section>
+      <section className="bg-white rounded-md px-6">
+        <div className="py-8 border-b-2">
+          <h2 className="text-3xl font-semibold pb-8 text-center">Our Team</h2>
+          <TeamCard />
+        </div>
+      </section>
+
+      <section className="bg-white rounded-md px-6">
+        <div className="text-2xl font-semibold text-center mb-4">
+          <h2 className="text-3xl py-8">Testimoni</h2>
+          <FeedbackCard />
+        </div>
+      </section>
     </div>
   );
 }
