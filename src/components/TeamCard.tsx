03b5efcdf40f5ac7011637b57a4d063144a7a7c2
paresiqbal <thecrowdel@gmail.com>
2023-12-16 10:19:25 +0700

    Add team profile picture

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,7 +2,6 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -10,19 +9,22 @@ import {
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
+// assets
+import rical from "@/assets/rical.jpg";
+import alifia from "@/assets/alifia.jpg";
+import adni from "@/assets/adni.jpg";
+import gisna from "@/assets/gisna.png";
+import thomas from "@/assets/thomas.jpg";
+
 export default function TeamCard() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
+            <AvatarImage src={rical} className="mx-auto object-cover" />
           </Avatar>
           <CardTitle className="font-custom font-thin">Rical</CardTitle>
-          <CardDescription>Leader</CardDescription>
         </CardHeader>
         <CardContent>
           <p>Cantik itu relatif, tergantung angle kamera, hehe.</p>
@@ -46,13 +48,9 @@ export default function TeamCard() {
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
+            <AvatarImage src={alifia} className="mx-auto object-cover" />
           </Avatar>
           <CardTitle className="font-custom font-thin">Aliffia</CardTitle>
-          <CardDescription>Writer</CardDescription>
         </CardHeader>
         <CardContent>
           <p>Tuhan pasti memberikan jalan buat kita, tapi belom dicor</p>
@@ -76,13 +74,9 @@ export default function TeamCard() {
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
+            <AvatarImage src={adni} className="mx-auto object-cover" />
           </Avatar>
           <CardTitle className="font-custom font-thin">Adni</CardTitle>
-          <CardDescription>Creative Director</CardDescription>
         </CardHeader>
         <CardContent>
           <p>Jangan berharap kepada tim SAR untuk menemukan jodohmu.</p>
@@ -106,13 +100,9 @@ export default function TeamCard() {
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
+            <AvatarImage src={gisna} className="mx-auto object-cover" />
           </Avatar>
           <CardTitle className="font-custom font-thin">Gisna</CardTitle>
-          <CardDescription>Facilitator</CardDescription>
         </CardHeader>
         <CardContent>
           <p>Life is too short. Smile while you still have teeth.</p>
@@ -136,13 +126,9 @@ export default function TeamCard() {
       <Card className="flex flex-col items-center text-center">
         <CardHeader className="flex flex-col items-center">
           <Avatar className="items-center">
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="mx-auto"
-            />
+            <AvatarImage src={thomas} className="mx-auto object-cover" />
           </Avatar>
           <CardTitle className="font-custom font-thin">Thomas</CardTitle>
-          <CardDescription>Coach</CardDescription>
         </CardHeader>
         <CardContent>
           <p>Segala hal indah pasti berawal dari mimpi, jadi ayo kita tidur.</p>
