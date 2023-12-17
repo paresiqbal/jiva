3d8aadc1dbfeac831a633a3ada5e014757267bf6
paresiqbal <thecrowdel@gmail.com>
2023-12-17 21:29:36 +0700

    Add firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsgzLOq1AlfbsJ0pghiD7L4aFzndo_EQ8",
+  authDomain: "jiva-b2940.firebaseapp.com",
+  projectId: "jiva-b2940",
+  storageBucket: "jiva-b2940.appspot.com",
+  messagingSenderId: "584561041880",
+  appId: "1:584561041880:web:ad4f002e7025d0154752ad",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
